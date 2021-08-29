af8fc1fbbf28440c3b87b65f0b49337c4661a50a
hadas21 <hadas.axel@gmail.com>
2021-08-28 22:28:06 -0500

    delete home from header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -31,7 +31,7 @@ const unauthenticatedOptions = (
 
 const alwaysOptions = (
   <Fragment>
-    <NavLink exact to='/' className='nav-link'>Home</NavLink>
+    {/* <NavLink exact to='/' className='nav-link'>Home</NavLink> */}
   </Fragment>
 )
 
